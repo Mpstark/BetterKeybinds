11be838c018d74008ad08b14f710d33e6f080a26
Michael P. Starkweather <mpstark@gmail.com>
2016-03-26 14:55:07 -0500

    Change structure a little

--- convertString.bat
+++ convertString.bat
@@ -1,4 +1,6 @@
 C:\Users\mpsta\Desktop\Witcher\w3strings\w3strings.exe -e en.csv -i 4573
 del en.csv.w3strings.ws
-del en.w3strings
 ren en.csv.w3strings en.w3strings
+del content\en.w3strings
+move en.w3strings content\en.w3strings
+del en.csv.w3strings
