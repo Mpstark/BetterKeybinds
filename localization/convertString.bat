c2110eb5552f45a942a48b7741c23086259d24ee
Michael P. Starkweather <mpstark@gmail.com>
2016-03-28 23:27:17 -0500

    Add localization for de and pl, thanks pawel1995

--- localization/convertString.bat
+++ localization/convertString.bat
@@ -2,3 +2,13 @@ C:\Users\mpsta\Desktop\Witcher\w3strings\w3strings.exe -e en.csv -i 4573
 del en.csv.w3strings.ws
 del ..\modBetterKeybinds\content\en.w3strings
 move en.csv.w3strings ..\modBetterKeybinds\content\en.w3strings
+
+C:\Users\mpsta\Desktop\Witcher\w3strings\w3strings.exe -e pl.csv -i 4573
+del pl.csv.w3strings.ws
+del ..\modBetterKeybinds\content\pl.w3strings
+move pl.csv.w3strings ..\modBetterKeybinds\content\pl.w3strings
+
+C:\Users\mpsta\Desktop\Witcher\w3strings\w3strings.exe -e de.csv -i 4573
+del de.csv.w3strings.ws
+del ..\modBetterKeybinds\content\de.w3strings
+move de.csv.w3strings ..\modBetterKeybinds\content\de.w3strings
